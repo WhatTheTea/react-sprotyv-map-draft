2dd436c08e7b4cccf95e8aeb94e822b75fd698e8
WhatTheTea <fox228.af@gmail.com>
2023-03-10 19:45:10 +0200

    mocking up the functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import React, { ReactNode } from 'react';
+import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 import './App.css';
 
 class App extends React.Component
 {
+  static readonly coords : Array<[number, number]> =
+  [
+    [51.50243870000001, 31.3110552],
+    [48.4435097, 26.8421332],
+    [48.2920787, 25.9358367]
+  ]
+  private children : Array<ReactNode> = []
+  TileLayer() : ReactNode
+  {
+  return <>
+    <TileLayer 
+          attribution='&copy; 
+          <a href="https://www.openstreetmap.org/copyright">
+          OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+  </>}
 
+  constructor({})
+  {
+    super({})
+    this.children.push(this.TileLayer())
+    this.children.push(MarkerNode(App.coords[0]))
+    this.children.push(MarkerNode(App.coords[1]))
+    this.children.push(MarkerNode(App.coords[2]))
+  }
   render() {
     return <>
     <MapContainer 
       center = {[49.32512, 30.9375]} 
       zoom={6}
-      id="map">
-        <TileLayer 
-          attribution='&copy; 
-          <a href="https://www.openstreetmap.org/copyright">
-          OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-    </MapContainer>
+      id="map"
+      children={this.children}/>
     </>
   }
+}
+
+function MarkerNode(coords:[number,number]) : ReactNode
+{
+  return <>
+    <Marker position={coords}/>
+  </>
 }
 
 export default App;
